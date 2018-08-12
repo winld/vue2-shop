980a8c3a13f86f3acfbf93901edbe596d41d40b9
lijun <792356522@qq.com>
2018-08-13 00:38:03 +0800

    goodlist组件拆分，修正helloword

---
+++ src/components/NavFooter.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer class="footer">
+    <div class="footer__wrap">
+      <div class="footer__secondary">
+        <div class="footer__inner">
+          <div class="footer__secondary__nav">
+            <span>Copyright©2013-2017 dongnaoedu.com All Rights Reserved.</span>
+            <a href="https://dongnaoedu.com/">
+              关于我们
+            </a>
+            <a href="http://dongnao.ke.qq.com/#tab=1&category=-1">
+              腾讯课堂&&动脑学院
+            </a>
+            <a href="https://dongnaoedu.com/index.html">
+              版权所有 2008-2017 湘ICP备16006411号-1
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+    export default {
+        name: 'NavFooter',
+        data() {
+            return {
+                msg: 'vue temp'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Navbread.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="nav-breadcrumb-wrap">
+    <div class="container">
+      <nav class="nav-breadcrumb">
+        <a href="/">主页</a>
+        <span>商品列表</span>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: 'vue temp'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
