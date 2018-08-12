63060545e9fb66728e92b73a00792f425d7f002a
lijun <792356522@qq.com>
2018-08-13 01:01:36 +0800

    准备数据源商品展示

--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -26,13 +26,13 @@
           <div class="accessory-list-wrap">
             <div class="accessory-list col-4">
               <ul>
-                <li>
+                <li v-for="item in goodsList">
                   <div class="pic">
-                    <a href="#"><img src="/static/1.jpg" alt=""></a>
+                    <a href="#"><img v-bind:src="'/static/'+item.productImage" alt=""></a>
                   </div>
                   <div class="main">
-                    <div class="name">小米电视4 55英寸</div>
-                    <div class="price">3999</div>
+                    <div class="name">{{item.productName}}</div>
+                    <div class="price">{{item.salePrice}}</div>
                     <div class="btn-area">
                       <a href="javascript:;" class="btn btn--m">加入购物车</a>
                     </div>
@@ -62,7 +62,12 @@
    // name: 'HelloWorld',
     data () {
       return {
-        msg: 'dhrdfthfghfg'
+        goodsList: [{
+          "productId":"10001",
+          "productName":"小米空气净化器 2",
+          "salePrice":"699",
+          "productImage":"小米空气净化器 2.jpg"
+        }]
       }
     },
     components:{
