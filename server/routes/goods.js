cdd73a54119730fefa3fa08ba875363b81dce336
lijun <792356522@qq.com>
2018-08-14 00:23:31 +0800

    商品列表分页服务端查询

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -18,7 +18,15 @@ mongoose.connection.on("disconnected", function () {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Goods.find({},function(err,doc){
+  //f分页查询
+  let page = parseInt(req.param("page"));
+  let pageSize = parseInt(req.param("pageSize"));
+  let sort = req.param("sort");
+  let skip = (page-1)*pageSize;
+  let params = {};
+  let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+  goodsModel.sort({'salePrice':sort});
+  goodsModel.exec(function (err, doc) {
     if(err){
       res.json({
         status:'1',
@@ -35,6 +43,24 @@ router.get('/', function(req, res, next) {
       })
     }
   })
+//非分页接口查询
+  /*Goods.find({},function(err,doc){
+    if(err){
+      res.json({
+        status:'1',
+        msg:err.message
+      })
+    }else{
+      res.json({
+        status:'1',
+        msg:'',
+        result:{
+          count:doc.length,
+          list:doc
+        }
+      })
+    }
+  })*/
 });
 
 module.exports = router;
